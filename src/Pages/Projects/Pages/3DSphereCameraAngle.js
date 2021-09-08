2bc44e6efb75e9121542b8aa67e74a41f8665af0
Dmitriy <d.komerzan@gmail.com>
2021-09-08 12:06:08 -0400

    updated web page

--- src/Pages/Projects/Pages/3DSphereCameraAngle.js
+++ src/Pages/Projects/Pages/3DSphereCameraAngle.js
@@ -11,7 +11,20 @@ export default function CameraAngle(props) {
             </Typography>
 
             <p>
-                This app demonstrates the different lighting you can put in a THREE JS Scene. I made the project in order to get a better idea on how lighting works in a scene. I have various lights with different colors pointing at the dice so you can differentiate the lights from one and other. You can also change the light colors.  <b> *** This application is currently being debugged.*** </b>
+                This app demonstrates the different lighting you can have in a THREE JS Scene. 
+                
+                I made the project in order to get a better idea on how lighting works in a scene.
+                 I have 3 different lights with different colors pointing at the dice so you can
+                  differentiate the lights from one and other. The lights are labeled accordingly in the scene. You can also change the color of the lights using the color palette in the top right corner.
+
+                  <ol>
+
+                       Types of Lights: 
+                      <li>Key Light - This is the main light that points toward the object of focus and generates strong shadows </li>
+                      <li>Fill Light - This is a dimmer light oppsite of the key light used to dampen the strong shadows created by the keylight </li>
+                      <li>Rim Light - A light behind the object of focus to help the subject pop out of the background</li>
+                 </ol>
+                    
             </p>
             <Divider />
             <Typography classes={{ root: "AboutSection" }} variant="h5" gutterBottom>
